8b11fa0f885b5032356d417adfcf18ab421fb8da
benjilabananee <fassi.benjamin@gmail.com>
2024-03-18 14:24:50 +0200

    create class processor

--- src/Mongodb_handler/mongo.py
+++ src/Mongodb_handler/mongo.py
@@ -14,12 +14,21 @@ class MongoDBManager:
         collection = self.db[collection_name]
         collection.insert_one(document)
 
-    def update_document(self, collection_name, key, new_value):
+    def update_document(self, key, value_to_search, replacement_value, collection_name):
         collection = self.db[collection_name]
-        update_operation = collection.update_one({key: {'$exists': True}}, {'$set': {key: new_value}})
-        print(update_operation)
+        query = {key: value_to_search}
+        update_query = {'$set': replacement_value}
+        print(value_to_search)
+        collection.update_one(query, update_query)
 
-    def find_document_by_key(self, collection_name, key: str):
+    def find_document_by_key(self, key, value_to_search, collection_name):
+        collection = self.db[collection_name]
+        existing_document = collection.find_one({key: value_to_search})
+        return existing_document
+
+    def find_document_by_dynamic_key(self, key, collection_name):
         collection = self.db[collection_name]
         existing_document = collection.find_one({key: {'$exists': True}})
         return existing_document
+
+
--- src/processor/processor.py
+++ src/processor/processor.py
@@ -3,12 +3,11 @@ import pandas as pd
 from src.Personn.employee import Employee
 from src.Mongodb_handler.mongo import MongoDBManager
 
-manager = MongoDBManager('localhost', 27017, 'my_database')
+mongo = MongoDBManager('localhost', 27017, 'my_database')
 
 
 class DataProcessor:
-    def __init__(self, input_file_directory: str, output_file_name_after_trans: str,
-                 output_file_dir_for_mutual_friends: str):
+    def __init__(self, input_file_directory: str, output_file_name_after_trans: str, output_file_dir_for_mutual_friends: str):
         self.input_file_directory = input_file_directory
         self.output_file_name_after_trans = output_file_name_after_trans
         self.json_data = self.extract_json_data()
@@ -24,14 +23,16 @@ class DataProcessor:
             json.dump(json_data, outfile, indent=2)
 
     def save_each_file_with_enrichment(self):
-        employee_list_json = []
         for person_data in self.json_data:
             employee = Employee(person_data)
             file_name = self.output_file_name_after_trans + f'{employee.guid}.json'
-            self.save_json_to_disk(file_name, vars(employee))  # convert the employee object into a dictionary
-            employee_list_json.append(employee)
-            print(f'{file_name} saved successfully as single employee file.')
-        return employee_list_json
+            self.save_json_to_disk(file_name, vars(employee))
+            if mongo.find_document_by_key('guid', employee.guid, 'employee_enrichments') is None:
+                mongo.insert_document('employee_enrichments', vars(employee))
+                print(f'new Document with guid {employee.guid} was inserted!!')
+            else:
+                mongo.update_document('guid', employee.guid, vars(employee), 'employee_enrichments')
+                print(f'new Document with guid {employee.guid} was updated!!')
 
     def find_mutual_friends(self):
         df = pd.DataFrame(self.json_data)
@@ -45,19 +46,24 @@ class DataProcessor:
                     key = '{}_{}'.format(df.iloc[i]['_id'], df.iloc[j]['_id'])
                     dict_to_insert = {key: list(mutual_friends)}
                     self.save_json_to_disk(self.output_file_dir_for_mutual_friends + key + '.json', dict_to_insert)
-
-                    if manager.find_document_by_key('users', key) is None:
-                        manager.insert_document('users', dict_to_insert)
-                        print('new values was inserted!')
+                    document_in_db = mongo.find_document_by_dynamic_key(key, 'mutual_friends')
+                    if document_in_db is not None:
+                        mongo.update_document(key, document_in_db[key],{key: list(mutual_friends)}, 'mutual_friends')
+                        print("is existing")
                     else:
-                        manager.update_document('users', key, list(mutual_friends))
-                        print('updated')
+                        mongo.insert_document('mutual_friends', dict_to_insert)
+                        print(key + " do not exist")
+
 
 if __name__ == "__main__":
-    manager = MongoDBManager('localhost', 27017, 'my_database')
+    mongo = MongoDBManager('localhost', 27017, 'my_database')
     process_data = DataProcessor(
         'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_transformed\\jsonEx.json',
         'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_loaded_into_mongo\\',
         'C:\\Users\\benji\\PycharmProjects\\pythonProject\\mutual_friend_save\\')
 
-    process_data.find_mutual_friends()
+    while(True):
+        process_data.find_mutual_friends()
+        process_data.save_each_file_with_enrichment()
+
+
