13365b1c0595cc34b6d6b0b387eb8e8d9c145129
benjilabananee <fassi.benjamin@gmail.com>
2024-03-18 21:04:23 +0200

    create class processor

--- src/Mongodb_handler/mongo.py
+++ src/Mongodb_handler/mongo.py
@@ -1,4 +1,3 @@
-from bson import ObjectId
 from pymongo import MongoClient, UpdateOne
 
 
@@ -11,23 +10,42 @@ class MongoDBManager:
         self.db = self.client[self.db_name]
 
     def insert_document(self, collection_name, document: dict):
-        collection = self.db[collection_name]
-        collection.insert_one(document)
+        try:
+            collection = self.db[collection_name]
+            collection.insert_one(document)
+        except Exception as e:
+            print(f"Error inserting document into {collection_name}: {e}")
 
     def update_document(self, key, value_to_search, replacement_value, collection_name):
-        collection = self.db[collection_name]
-        query = {key: value_to_search}
-        update_query = {'$set': replacement_value}
-        collection.update_one(query, update_query)
+        try:
+            collection = self.db[collection_name]
+            query = {key: value_to_search}
+            update_query = {'$set': replacement_value}
+            collection.update_one(query, update_query)
+        except Exception as e:
+            print(f"Error updating document in {collection_name}: {e}")
 
     def find_document_by_key(self, key, value_to_search, collection_name):
-        collection = self.db[collection_name]
-        existing_document = collection.find_one({key: value_to_search})
-        return existing_document
+        try:
+            collection = self.db[collection_name]
+            existing_document = collection.find_one({key: value_to_search})
+            return existing_document
+        except Exception as e:
+            print(f"Error finding document by key in {collection_name}: {e}")
+            return None
 
     def find_document_by_dynamic_key(self, key, collection_name):
-        collection = self.db[collection_name]
-        existing_document = collection.find_one({key: {'$exists': True}})
-        return existing_document
+        try:
+            collection = self.db[collection_name]
+            existing_document = collection.find_one({key: {'$exists': True}})
+            return existing_document
+        except Exception as e:
+            print(f"Error finding document by dynamic key in {collection_name}: {e}")
+            return None
 
-
+    def close_connection(self):
+        try:
+            self.client.close()
+            print("MongoDB connection closed.")
+        except Exception as e:
+            print(f"Error closing MongoDB connection: {e}")
--- src/processor/processor.py
+++ src/processor/processor.py
@@ -1,73 +1,111 @@
 import json
+import os
 import pandas as pd
 from src.Personn.employee import Employee
 from src.Mongodb_handler.mongo import MongoDBManager
 
 
-
 class DataProcessor:
     def __init__(self, input_file_directory: str, output_file_name_after_trans: str,
-                 output_file_dir_for_mutual_friends: str):
+                 output_file_dir_for_mutual_friends: str, mongo: MongoDBManager):
         self.input_file = input_file_directory
         self.output_file_name_after_trans = output_file_name_after_trans
-        self.json_data = self._extract_json_data()
         self.output_file_dir_for_mutual_friends = output_file_dir_for_mutual_friends
+        self.mongo = mongo
 
     def _extract_json_data(self):
-        with open(self.input_file, 'r') as file:
-            return json.load(file)
+        try:
+            with open(self.input_file, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"Input file '{self.input_file}' not found.")
+            return None
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON file '{self.input_file}'.")
+            return None
 
-    @staticmethod
-    def _save_json_to_disk(file_name: str, json_data: dict):
-        with open(file_name, 'w') as outfile:
-            json.dump(json_data, outfile, indent=2)
+    def _save_json_to_disk(self, file_name: str, json_data: dict):
+        try:
+            with open(file_name, 'w') as outfile:
+                json.dump(json_data, outfile, indent=2)
+        except Exception as e:
+            print(f"Error saving JSON data to file '{file_name}': {e}")
 
     def _delete_json_file_from_disk(self, file_name: str):
-        pass
+        try:
+            os.remove(file_name)
+        except FileNotFoundError:
+            print(f"File '{file_name}' not found.")
+        except Exception as e:
+            print(f"Error deleting file '{file_name}': {e}")
 
     def save_each_file_with_enrichment(self):
-        for person_data in self.json_data:
-            employee = Employee(person_data)
-            self._save_json_to_disk(self.output_file_name_after_trans + f'{employee.guid}.json', vars(employee))
-            if mongo.find_document_by_key('guid', employee.guid, 'employee_enrichments') is None:
-                mongo.insert_document('employee_enrichments', vars(employee))
-                print(f'new Document with guid {employee.guid} was inserted!!')
-            else:
-                mongo.update_document('guid', employee.guid, vars(employee), 'employee_enrichments')
-                print(f'new Document with guid {employee.guid} was updated!!')
+        json_data = self._extract_json_data()
+        if not json_data:
+            return
 
-            self._delete_json_file_from_disk(self.input_file)
+        for person_data in json_data:
+            try:
+                employee = Employee(person_data)
+                file_path = os.path.join(self.output_file_name_after_trans, f'{employee.guid}.json')
+                self._save_json_to_disk(file_path, vars(employee))
+
+                existing_doc = self.mongo.find_document_by_key('guid', employee.guid, 'employee_enrichments')
+                if existing_doc:
+                    self.mongo.update_document('guid', employee.guid, vars(employee), 'employee_enrichments')
+                    print(f'Document with guid {employee.guid} updated.')
+                else:
+                    self.mongo.insert_document('employee_enrichments', vars(employee))
+                    print(f'New document with guid {employee.guid} inserted.')
+
+                # self._delete_json_file_from_disk(self.input_file)
+            except Exception as e:
+                print(f"Error processing employee data: {e}")
 
     def find_mutual_friends(self):
-        df = pd.DataFrame(self.json_data)
+        json_data = self._extract_json_data()
+        if not json_data:
+            return
+
+        df = pd.DataFrame(json_data)
 
         for i in range(len(df)):
             for j in range(i + 1, len(df)):
-                friends1 = set([item['name'] for item in df['friends'][i]])
-                friends2 = set([item['name'] for item in df['friends'][j]])
-                mutual_friends = friends1.intersection(friends2)
-                if mutual_friends:
-                    key = '{}_{}'.format(df.iloc[i]['_id'], df.iloc[j]['_id'])
-                    dict_to_insert = {key: list(mutual_friends)}
-                    self._save_json_to_disk(self.output_file_dir_for_mutual_friends + key + '.json', dict_to_insert)
-                    document_in_db = mongo.find_document_by_dynamic_key(key, 'mutual_friends')
-                    if document_in_db is not None:
-                        mongo.update_document(key, document_in_db[key], {key: list(mutual_friends)}, 'mutual_friends')
-                        print(f'document {key} exist on db schema mutual friends and was updated')
-                    else:
-                        mongo.insert_document('mutual_friends', dict_to_insert)
-                        print(f'new document {key} was inserted on db schema mutual_friends!')
+                try:
+                    friends1 = set([item['name'] for item in df['friends'][i]])
+                    friends2 = set([item['name'] for item in df['friends'][j]])
+                    mutual_friends = friends1.intersection(friends2)
+                    if mutual_friends:
+                        key = '{}_{}'.format(df.iloc[i]['_id'], df.iloc[j]['_id'])
+                        dict_to_insert = {key: list(mutual_friends)}
+                        file_path = os.path.join(self.output_file_dir_for_mutual_friends, f'{key}.json')
+                        self._save_json_to_disk(file_path, dict_to_insert)
 
-        self._delete_json_file_from_disk(self.input_file)
+                        existing_doc = self.mongo.find_document_by_dynamic_key(key, 'mutual_friends')
+                        if existing_doc:
+                            self.mongo.update_document(key, existing_doc[key], {key: list(mutual_friends)},
+                                                       'mutual_friends')
+                            print(f'Document {key} updated.')
+                        else:
+                            self.mongo.insert_document('mutual_friends', dict_to_insert)
+                            print(f'New document {key} inserted.')
+                except Exception as e:
+                    print(f"Error finding mutual friends: {e}")
+
+        # self._delete_json_file_from_disk(self.input_file)
 
 
 if __name__ == "__main__":
-    mongo = MongoDBManager('localhost', 27017, 'my_database')
-    process_data = DataProcessor(
-        'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_transformed\\jsonEx.json',
-        'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_loaded_into_mongo\\',
-        'C:\\Users\\benji\\PycharmProjects\\pythonProject\\mutual_friend_save\\')
+    try:
+        mongo = MongoDBManager('localhost', 27017, 'my_database')
+        process_data = DataProcessor(
+            'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_transformed\\jsonEx.json',
+            'C:\\Users\\benji\\PycharmProjects\\pythonProject\\file_to_be_loaded_into_mongo\\',
+            'C:\\Users\\benji\\PycharmProjects\\pythonProject\\mutual_friend_save\\',
+            mongo)
 
-    while (True):
-        process_data.find_mutual_friends()
-        process_data.save_each_file_with_enrichment()
+        while True:
+            process_data.find_mutual_friends()
+            process_data.save_each_file_with_enrichment()
+    except Exception as e:
+        print(f"An error occurred: {e}")
